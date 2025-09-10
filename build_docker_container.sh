c88bd633120ee0cc8667315817be9fe80eacd12a
ironkrendel <v.a.ryshkov@gmail.com>
2025-09-10 21:27:46 +0300

    ssl cert test

--- build_docker_container.sh
+++ build_docker_container.sh
@@ -1,1 +1,2 @@
+openssl req -x509 -newkey rsa:4096 -keyout www.kasaneteto.ru.key -out www.kasaneteto.ru.crt -sha256 -days 3650 -nodes -subj "/C=ND/ST=Netherlands/L=Amsterdam/O=ironkrendel/OU=kasaneteto.ru/CN=kasaneteto.ru"
 docker build ./ -t year1_practice
